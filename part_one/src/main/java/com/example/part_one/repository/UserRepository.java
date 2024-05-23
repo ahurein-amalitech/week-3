6e8891102f390e8adb41ca5b06ccc2f2d4697246
ahurein-amalitech <ebenezer.ahurein@amalitech.com>
2024-05-23 13:41:18 +0000

    feat: complete part one

---
+++ part_one/src/main/java/com/example/part_one/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.part_one.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+    public String getUser(){
+        return "Ahurein";
+    }
+}
